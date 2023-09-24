513e8395a8ce2bbfe6da240bf5471e526fac6b95
sujissi_Laptop <hwangsuji329@gmail.com>
2023-09-24 20:37:07 +0900

    사각형 윗줄 라인

--- character_circle_rectangle_move.py
+++ character_circle_rectangle_move.py
@@ -30,12 +30,12 @@ def run_rectangle():
         render_frame(x,90)
         
     for x in range(750,50-1,-10):
-        render_frame(x,90)
+        render_frame(x,550)
     
     pass
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
 
